18c68359522bec051230c03b5bb02002dc4f5ed1
markpatel13 <markpatel044@gmail.com>
2025-07-31 09:29:14 +0530

    final commit

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {registerUser,loginUser, userCredits} from "../controllers/userControllers.js";
+import {registerUser,loginUser, userCredits, paymentRazorpay,verifyRazorpay} from "../controllers/userControllers.js";
 import userAuth from "../middlewares/auth.js";
 
 const userRouter = express.Router()
@@ -8,6 +8,8 @@ userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/credits",userAuth,userCredits);
 userRouter.post("/credits",userAuth,userCredits);
+userRouter.post("/pay-razor",userAuth,paymentRazorpay);
+userRouter.post("/verify-razor",verifyRazorpay);
 
 export default userRouter;
 
